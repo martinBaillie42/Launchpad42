ec89773489443be4f01aed74c2e5ee24fbe1dda1
martin <notinventedhere8@gmail.com>
2017-05-20 23:34:38 +0100

    Bash run script checks if Live is running before quitting. py run script created so bash run script can be run from Pycharm CE.

--- run.sh
+++ run.sh
@@ -3,16 +3,18 @@ if [ -f '*.pyc' ]; then
    rm *.pyc
 fi
 
-osascript -e 'tell application "System Events"
-	set frontmost of process "Live" to true
-	keystroke "q" using command down
-	delay 0.1
-	tell application "System Events" to keystroke (ASCII character 28) --left arrow
-	delay 0.1
-	tell application "System Events" to keystroke (ASCII character 28) --left arrow
-	delay 0.1
-	keystroke return
-end tell'
+if pgrep Live; then
+    osascript -e 'tell application "System Events"
+        set frontmost of process "Live" to true
+        keystroke "q" using command down
+        delay 0.1
+        tell application "System Events" to keystroke (ASCII character 28) --left arrow
+        delay 0.1
+        tell application "System Events" to keystroke (ASCII character 28) --left arrow
+        delay 0.1
+        keystroke return
+    end tell'
+fi
 
 while pgrep Live; do
   sleep 1
