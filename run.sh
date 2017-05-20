ea5c540fc4dae608fe367f9ee2a3ca66c654c1e9
martin <notinventedhere8@gmail.com>
2017-05-20 23:45:52 +0100

    Delays in osascript proved unnecessary. Removed.

--- run.sh
+++ run.sh
@@ -4,11 +4,8 @@ if pgrep Live; then
     osascript -e 'tell application "System Events"
         set frontmost of process "Live" to true
         keystroke "q" using command down
-        delay 0.1
         tell application "System Events" to keystroke (ASCII character 28) --left arrow
-        delay 0.1
         tell application "System Events" to keystroke (ASCII character 28) --left arrow
-        delay 0.1
         keystroke return
     end tell'
 fi
